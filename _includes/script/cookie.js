9e1b18fb1cb64d492a0b3c34c333237d2879c7b9
Sunny S <3030712@gmail.com>
2015-09-22 18:17:35 -0700

    css test.

--- _includes/script/cookie.js
+++ _includes/script/cookie.js
@@ -6,7 +6,7 @@ var ca = document.cookie.split(';');
 var cname = name + "=";
   for (var i=0; i<ca.length; ++i){
   var ch = ca[i];
-    while (c.charAt(0)==' '){
+    while (ch.charAt(0)==' '){
     ch = ch.substring(1);
       if (ch.indexOf(cname) == 0)
       return ch.substring(cname.length, ch.length);
